de7213dd089532ac007c45d35be135c46f85f134
MoellerO <oliver.moeller@die-olchies.de>
2022-10-28 20:27:28 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,26 +4,46 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup as soup
+import time
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(service=Service(
     ChromeDriverManager().install()), options=options)
 
-driver.get("https://www.twitch.tv/tolkinlol")
+driver.get("https://www.twitch.tv/montanablack88")
 
 # ScAnimatedNumber-sc-acnd2k-0
 # CoreText-sc-cpl358-0
 
 try:
-    element = WebDriverWait(driver, 10).until(
+    view_count = WebDriverWait(driver, 20).until(
         EC.presence_of_element_located(
             (By.CSS_SELECTOR, ".ScAnimatedNumber-sc-acnd2k-0.cIxmzd"))
     )
-    # view_count = element.find
-    print(element.text)
-except:
-    print("Failed")
+    _ = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located(
+            (By.CLASS_NAME, "chat-line__message"))
+    )
+    print('[LOG] Found all required elements')
+    # chat = driver.find_element(By.CSS_SELECTOR, '.chat-line__message')
+    # chat = driver.find_element(
+    #     By.CSS_SELECTOR, '.Layout-sc-nxg1ff-0.aleoz.chat-scrollable-area__message-container :last-child')
+    # print(chat.text)
+    while True:
+        html = driver.page_source
+        page_soup = soup(html, features='html.parser')
+        last_message = page_soup.find_all(
+            'div', {'class': 'chat-line__message'})[-1].get_text()
+        view_count = page_soup.find(
+            'span', {'class': ['ScAnimatedNumber-sc-acnd2k-0', 'cIxmzd']}).get_text()
+        print("[Last Message/View Count]:", last_message, view_count)
+        time.sleep(1)
+
+
+except Exception as e:
+    print(e)
     driver.quit()
 
 

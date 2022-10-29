3002acd7d03430a8cfe047ce1a887db9c5bd9a54
altansnl <altansenel@sabanciuniv.edu>
2022-10-29 13:19:13 +0200

    live sentiment from twitch chat

--- scraper.py
+++ scraper.py
@@ -3,35 +3,42 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup as soup
 import time
-from nltk.stem import WordNetLemmatizer
+from model import sentimentAnalyzeSentence
 
-lemmatizer = WordNetLemmatizer()
+TWITCH_USERNAME = 'dota2ti' # channel for analysis
+TICK_FREQUENCY = 1.0 # message pulling rate
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+if __name__ == "__main__":
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.get(f"https://www.twitch.tv/{TWITCH_USERNAME}")
 
-twitch_username = 'hasanabi'
-driver.get(f"https://www.twitch.tv/{twitch_username}")
+    last_tuple = 'text'
+    while True:
+        html = driver.page_source
+        page_soup = soup(html, features='html.parser')
+        tuples = page_soup.find_all(
+            'div', {'class': 'chat-line__message'})
+        tuples = [m.get_text() for m in tuples]
+        idx = 0
+        try:
+            idx = tuples.index(last_tuple)
+            tuples = tuples[idx+1:]
+        except:
+            pass
+        if len(tuples) > 0:
+            last_tuple = tuples[-1]
+            for tple in tuples:
+                message = tple.split(':')[1]
+                message = message.strip()
+                if message != '':
+                    sentiment = sentimentAnalyzeSentence(message)
 
-last_tuple = 'text'
-while True:
-    html = driver.page_source
-    page_soup = soup(html, features='html.parser')
-    tuples = page_soup.find_all(
-        'div', {'class': 'chat-line__message'})
-    tuples = [m.get_text() for m in tuples]
-    idx = 0
-    try:
-        idx = tuples.index(last_tuple)
-        tuples = tuples[idx+1:]
-    except:
-        pass
-    if len(tuples) > 0:
-        last_tuple = tuples[-1]
-        for tple in tuples:
-            message = tple.split(':')[1]
-            message = message.strip()
-            if message != '':
-                print(message)
-    time.sleep(0.25)
+                    # neutral 1.0 means 0 information about the messages sentiment
+                    # good to discard these messages
+                    if sentiment["neu"] != 1.0:
+                        print(message)
+                        print(str(sentiment))
+                        print("\n")
+        time.sleep(TICK_FREQUENCY)

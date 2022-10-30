bf95aca8c924fac00c41b2fc6580f4ba6d3a677b
Ayberk <44614567+ayberkuckun@users.noreply.github.com>
2022-10-30 02:42:07 +0200

    latest

--- test.py
+++ test.py
@@ -10,10 +10,6 @@ def byteToString(byte):
 my_udf = F.UserDefinedFunction(byteToString, T.StringType())
 my_udf2 = F.UserDefinedFunction(sentimentAnalyzeSentence, T.DoubleType())
 
-
-
-print(f"Dummy Score: {sentimentAnalyzeSentence(['Dummy', 'Starting', 'Point'])} Model is Loaded.")
-
 # os.environ['HADOOP_HOME'] = 'C://Users//altan//Desktop//winutils-master//hadoop-2.6.0'
 # os.environ["JAVA_HOME"] = "C://Program Files//Java//jdk-19"
 # os.environ["PYSPARK_PYTHON"] = "python"
@@ -40,21 +36,27 @@ print("CHECKPOINT - 1")
 df = df.withColumn('message', my_udf('value'))
 df = df.drop('value')
 df = df.withColumn('sentiment_score', my_udf2('message'))
+df = df.filter(F.col("sentiment_score") != 0.0)
 
-df = (df 
+df = (df
     .withWatermark("timestamp", "1 minute")
-    .groupBy([F.window('timestamp', '2 minute', '1 minute')])
+    .groupBy(F.window('timestamp', '2 minute', '1 minute'),
+             df.topic
+             )
     .agg(
          {'sentiment_score': 'mean'}
     )
 )
 
-df = df \
-    .filter(F.col("sentiment_score") != 0.0) \
+print("printing...")
+
+df = df.select('window.start', 'window.end', 'Avg(sentiment_score)') \
     .writeStream \
-    .outputMode('append') \
-    .format('console') \
+    .format('csv') \
+    .option("path", "output/") \
+    .option("checkpointLocation", "checkpoint/") \
     .start().awaitTermination()
 
-# .option('truncate', False) \
+#     .option('truncate', False) \
+
 print("CHECKPOINT - 2")
